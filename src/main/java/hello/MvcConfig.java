ec520889ce2e9a718859aca2236451257009c13f
EDeoMa <edeoma@gmail.com>
2017-06-11 22:20:40 +0300

    Initial commit

---
+++ src/main/java/hello/MvcConfig.java
@@ -0,0 +1,28 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    PersonRepository personRepository;
+
+    @RequestMapping("/")
+    public ModelAndView peopleList() {
+        return new ModelAndView("home", "people", personRepository.findAll());
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/hello").setViewName("hello");
+    }
+}
